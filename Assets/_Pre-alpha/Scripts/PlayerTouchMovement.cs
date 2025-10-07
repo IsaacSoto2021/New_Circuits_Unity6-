9f484b1488c10ec25f8e1b3a1b24a995b7afd63b
PkKev67 <oajohnson8@gmail.com>
2025-10-07 00:32:14 -0700

    Input system changes .1

---
+++ Assets/_Pre-alpha/Scripts/PlayerTouchMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem.EnhancedTouch;
+
+public class PlayerTouchMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 _joyStickSize = new Vector2(300, 300);
+    //[SerializeField]
+    //private FloatingJoystick _joystick;
+
+
+
+
+
+}
