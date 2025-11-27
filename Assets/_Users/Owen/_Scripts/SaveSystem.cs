366925ac08b5489ac8cd87a67b9e98e1b9101069
PkKev67 <oajohnson8@gmail.com>
2025-11-26 17:04:18 -0800

    gregless

---
+++ Assets/_Users/Owen/_Scripts/SaveSystem.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+public static class SaveSystem
+{
+    public const string FILENAME_SAVEDATA = "/savedata.json";
+
+
+}
+
+[Serializable]
+public class PlayerInfo
+{
+    [SerializeField] public List<>
+
+}
